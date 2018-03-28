a6f11f239d84b10fa1ea29218118d073dd39fd96
Nick Gottschlich <nickpgott@gmail.com>
2018-03-27 21:45:28 -0500

    now deleted reddit comments

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -1,18 +1,22 @@
 import praw
 from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
 
-USER_AGENT='Social Scrubber: v0.0.1 (by /u/JavaOffScript)'
+USER_AGENT = 'Social Scrubber: v0.0.1 (by /u/JavaOffScript)'
 
-reddit = praw.Reddit(client_id=CLIENT_ID,
-                     client_secret=CLIENT_SECRET,
-                     user_agent=USER_AGENT,
-                     username=REDDIT_USERNAME,
-                     password=REDDIT_PASSWORD)
+reddit = praw.Reddit(
+  client_id=CLIENT_ID,
+  client_secret=CLIENT_SECRET,
+  user_agent=USER_AGENT,
+  username=REDDIT_USERNAME,
+  password=REDDIT_PASSWORD
+)
 
 # print(reddit.read_only)  # Output: False
 # print(reddit.user)
 
-# print(reddit.redditor('socialScrubberTest').comments)
+user = reddit.redditor(REDDIT_USERNAME)
 
-for comment in reddit.redditor('socialScrubberTester').comments.new(limit=None):
-    print(comment.body.split('\n', 1)[0][:79])
+for comment in user.comments.new(limit=None):
+  print(comment.body)
+  comment.delete()
+  print(f'Comment `{comment.body}` Deleted`')
