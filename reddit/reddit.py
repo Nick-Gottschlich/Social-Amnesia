5bb8a9dbd4bb81464cc4fceffb49574a5c56cbe0
Nick Gottschlich <nickpgott@gmail.com>
2018-03-29 19:41:51 -0500

    can find and delete a users submissions

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -10,13 +10,26 @@ reddit = praw.Reddit(
   username=REDDIT_USERNAME,
   password=REDDIT_PASSWORD
 )
+user = reddit.redditor(REDDIT_USERNAME)
 
 # print(reddit.read_only)  # Output: False
 # print(reddit.user)
 
-user = reddit.redditor(REDDIT_USERNAME)
+# Get and delete comments
+def deleteComments(user):
+  for comment in user.comments.new(limit=None):
+    print(comment.body)
+    # comment.delete()
+    # print(f'Comment `{comment.body}` Deleted`')
 
-for comment in user.comments.new(limit=None):
-  print(comment.body)
-  comment.delete()
-  print(f'Comment `{comment.body}` Deleted`')
+# Get and delete submissions
+def deleteSubmissions(user):
+  print (user.submissions)
+  for submission in user.submissions.new(limit=None):
+    # print(submission.title)
+    # submission.delete()
+    # print(f'Comment `{comment.body}` Deleted`')
+
+# deletecomments(user)
+# deleteSubmissions(user)
+
