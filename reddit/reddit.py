1f496826a0fa6c59a65383d5cd41f99d9a412843
Nick Gottschlich <nickpgott@gmail.com>
2018-03-31 15:41:20 -0500

    Created a basic UI to fire delete comments and delete submissions scripts

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -1,4 +1,5 @@
 import praw
+import tkinter as tk
 from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
 
 USER_AGENT = 'Social Scrubber: v0.0.1 (by /u/JavaOffScript)'
@@ -16,20 +17,45 @@ user = reddit.redditor(REDDIT_USERNAME)
 # print(reddit.user)
 
 # Get and delete comments
-def deleteComments(user):
+def deleteComments():
   for comment in user.comments.new(limit=None):
     print(comment.body)
     # comment.delete()
     # print(f'Comment `{comment.body}` Deleted`')
 
 # Get and delete submissions
-def deleteSubmissions(user):
-  print (user.submissions)
+def deleteSubmissions():
   for submission in user.submissions.new(limit=None):
-    # print(submission.title)
+    print(submission.title)
     # submission.delete()
-    # print(f'Comment `{comment.body}` Deleted`')
+    # print(f'Comment `{submission.title}` Deleted`')
 
 # deletecomments(user)
 # deleteSubmissions(user)
 
+def createUI():
+  root = tk.Tk()
+  frame = tk.Frame(root)
+  frame.pack()
+
+  deleteCommentsButton = tk.Button(
+    frame,
+    text='Delete comments',
+    fg='red',
+    command=deleteComments)
+  deleteCommentsButton.pack(side=tk.LEFT)
+
+  deleteSubmissionsButton = tk.Button(
+    frame,
+    text='Delete submissions',
+    fg='red',
+    command=deleteSubmissions)
+  deleteSubmissionsButton.pack(side=tk.LEFT)
+
+  root.mainloop()
+
+def main():
+  createUI()
+
+if __name__ == "__main__":
+  main()
---
+++ tkinterTest/tkTest.py
@@ -0,0 +1,109 @@
+import tkinter as tk
+
+def helloWorld():
+  #root creates the root widget, aka the window that pops up
+  root = tk.Tk()
+
+  # creates the label widget, attaches it to root, gives it text
+  w = tk.Label(root, text="Hello Tkinter!")
+  #tells Tk to fit size of window to given text
+  w.pack()
+  #window won't appear until we enter the Tkinter event loop
+  root.mainloop()
+
+def images():
+  root = tk.Tk()
+  logo = tk.PhotoImage(file="test.gif")
+
+  explanation = """At present, only GIF and PPM/PGM
+  formats are supported, but an interface 
+  exists to allow additional image file
+  formats to be added easily."""
+
+  # tk.Label(root,
+    # justify=tk.CENTER,
+    # padx=10,
+    # text=explanation).pack(side="right")
+  # tk.Label(root, image=logo).pack(side="left")
+
+  tk.Label(
+    root,
+    justify = tk.LEFT,
+    compound = tk.LEFT,
+    padx = 10,
+    text = explanation,
+    image = logo
+  ).pack(side="right")
+
+  root.mainloop()
+
+def labels():
+  import tkinter as tk
+
+  root = tk.Tk()
+  tk.Label(root,
+          text="Red Text in Times Font",
+          fg="red",
+          padx=20,
+          font="Times").pack()
+  tk.Label(root,
+          text="Green Text in Helvetica Font",
+          fg="light green",
+          bg="dark green",
+          font="Helvetica 32 bold italic").pack()
+  tk.Label(root,
+          text="Blue Text in Verdana bold",
+          fg="blue",
+          bg="yellow",
+          font="Verdana 10 bold").pack()
+
+  root.mainloop()
+
+# counter = 0 
+# def counter_label(label):
+#   def count():
+#     global counter
+#     counter += 1
+#     label.config(text=str(counter))
+#     label.after(1000, count)
+#   count()
+
+
+# root = tk.Tk()
+# root.title("Counting Seconds")
+# label = tk.Label(root, fg="green")
+# label.pack()
+# counter_label(label)
+# button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+# button.pack()
+# root.mainloop()
+
+def messageWidget():
+  master = tk.Tk()
+  whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.\n(Mahatma Gandhi)"
+  msg = tk.Message(master, text=whatever_you_do)
+  msg.config(bg='lightgreen', font=('times', 24, 'italic'))
+  msg.pack()
+  tk.mainloop()
+
+def buttons():
+  def write_slogan():
+    print("Tkinter is easy to use!")
+
+  root = tk.Tk()
+  frame = tk.Frame(root)
+  frame.pack()
+
+  button = tk.Button(frame,
+                    text="QUIT",
+                    fg="red",
+                    command=quit)
+  button.pack(side=tk.LEFT)
+  slogan = tk.Button(frame,
+                    text="Hello",
+                    command=write_slogan)
+  slogan.pack(side=tk.LEFT)
+
+  root.mainloop()
+
+buttons()
