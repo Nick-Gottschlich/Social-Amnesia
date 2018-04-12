397b1d10f6d070a08265e8613e194f20577907e7
Nick Gottschlich <nickpgott@gmail.com>
2018-04-11 20:44:59 -0500

    added a login tab, can now log in to reddit right through it

--- reddit/main.py
+++ reddit/main.py
@@ -3,34 +3,45 @@ from tkinter import *
 from tkinter import ttk
 import arrow
 
-from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
+# from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
 
 USER_AGENT = 'Social Scrubber: v0.0.1 (by /u/JavaOffScript)'
 
 # hardcoded for now, this will eventually be set by user, somehow
 reddit = praw.Reddit(
-  client_id=CLIENT_ID,
-  client_secret=CLIENT_SECRET,
-  user_agent=USER_AGENT,
-  username=REDDIT_USERNAME,
-  password=REDDIT_PASSWORD
+  client_id='',
+  client_secret='',
+  user_agent='',
+  username='',
+  password=''
 )
 
-#lets have a python dictionary that will hold state stuff, that will eventually be factored out to a different file
-state = {}
+# the reddit object, for praw
+# reddit = praw.Reddit()
 
-def setInitState():
-  state['user'] = reddit.redditor(REDDIT_USERNAME)
-  state['recentlyPostedCutoff'] = arrow.now().replace(hours=0)
-  state['maxScore'] = 0
+#lets have a python dictionary that will hold redditState stuff, that will eventually be factored out to a different file
+redditState = {}
 
-
-# prints out the state to console
+# prints out the redditState to console
 def printState():
-  print(state)
+  print(redditState)
 
 
-# Sets the hours of comments or submissions to save, stores it in state
+def setRedditLogin(username, password, clientID, clientSecret):
+  reddit = praw.Reddit(
+    client_id=clientID,
+    client_secret=clientSecret,
+    user_agent=USER_AGENT,
+    username=username,
+    password=password
+  )
+
+  redditState['user'] = reddit.redditor(username)
+  redditState['recentlyPostedCutoff'] = arrow.now().replace(hours=0)
+  redditState['maxScore'] = 0
+
+
+# Sets the hours of comments or submissions to save, stores it in redditState
 #  and updates the UI to show what its currently set to.
 # hoursToSave: the input recieved from the UI
 # currentHoursToSave: what is stored for the user in the UI
@@ -40,7 +51,7 @@ def setHoursToSave(hoursToSave, currentHoursToSave):
   else:
     hoursToSave = int(hoursToSave)
 
-  state['recentlyPostedCutoff'] = arrow.now().replace(hours=-hoursToSave)
+  redditState['recentlyPostedCutoff'] = arrow.now().replace(hours=-hoursToSave)
   currentHoursToSave.set(f'Currently set to: {str(hoursToSave)} hours')
 
 
@@ -52,16 +63,16 @@ def setMaxScore(maxScore, currentMaxScore):
   if (maxScore == ''):
     maxScore = 0
   elif (maxScore == 'Unlimited'):
-    state['maxScore'] = 9999999999
+    redditState['maxScore'] = 9999999999
   else:
     maxScore = int(maxScore)
-    state['maxScore'] = maxScore
+    redditState['maxScore'] = maxScore
   
   
   currentMaxScore.set(f'Currently set to: {str(maxScore)} upvotes')
 
 
-# checks items against possibly whitelisted conditions defined in state, and either skips or deletes
+# checks items against possibly whitelisted conditions defined in redditState, and either skips or deletes
 def checkWhiteList(item, commentBool):
   if commentBool: 
     itemString = 'Comment' 
@@ -72,9 +83,9 @@ def checkWhiteList(item, commentBool):
 
   timeCreated = arrow.get(item.created_utc)
 
-  if (timeCreated > state['recentlyPostedCutoff']):
+  if (timeCreated > redditState['recentlyPostedCutoff']):
     print(f'{itemString} `{itemSnippet}` is more recent than cutoff. skipping')
-  elif (item.score > state['maxScore']):
+  elif (item.score > redditState['maxScore']):
     print (f'{itemString} `{itemSnippet}` is higher than max score, skipping')
   else:
     # comment back in once things get real
@@ -85,14 +96,50 @@ def checkWhiteList(item, commentBool):
 
 # Get and delete comments
 def deleteComments():
-  for comment in state['user'].comments.new(limit=None):
+  for comment in redditState['user'].comments.new(limit=None):
     checkWhiteList(comment, True)
 
 
 # Get and delete submissions
 def deleteSubmissions():
-  for submission in state['user'].submissions.new(limit=None):
+  for submission in redditState['user'].submissions.new(limit=None):
     checkWhiteList(submission, False)
+
+
+def buildLoginTab(loginFrame):
+  loginFrame.grid()
+
+  redditLabel = Label(loginFrame, text='reddit')
+  
+  redditUsernameLabel = Label(loginFrame, text='Enter reddit username:')
+  redditUsernameEntry = Entry(loginFrame)
+
+  redditPasswordLabel = Label(loginFrame, text='Enter reddit password:')
+  redditPasswordEntry = Entry(loginFrame)
+
+  redditClientIDLabel = Label(loginFrame, text='Enter reddit client ID:')
+  redditClientIDEntry = Entry(loginFrame)
+
+  redditClientSecretLabel = Label(loginFrame, text='Enter reddit client secret:')
+  redditClientSecretEntry = Entry(loginFrame)
+
+  redditLoginButton = Button(
+    loginFrame,
+    text='Login to reddit',
+    command=lambda: setRedditLogin(redditUsernameEntry.get(), redditPasswordEntry.get(), 
+      redditClientIDEntry.get(), redditClientSecretEntry.get())
+  )
+
+  redditLabel.grid(row=0, column=0)
+  redditUsernameLabel.grid(row=1, column=0)
+  redditUsernameEntry.grid(row=1, column=1)
+  redditPasswordLabel.grid(row=2, column=0)
+  redditPasswordEntry.grid(row=2, column=1)
+  redditClientIDLabel.grid(row=3, column=0)
+  redditClientIDEntry.grid(row=3, column=1)
+  redditClientSecretLabel.grid(row=4, column=0)
+  redditClientSecretEntry.grid(row=4, column=1)
+  redditLoginButton.grid(row=5, column=0)
 
 
 def buildRedditTab(redditFrame):
@@ -101,48 +148,48 @@ def buildRedditTab(redditFrame):
   currentHoursToSave = StringVar()
   currentHoursToSave.set('Currently set to: 0 hours')
   hoursTextLabel = Label(
-      redditFrame, text='Hours of comments/submissions to keep:')
+    redditFrame, text='Hours of comments/submissions to keep:')
   hoursEntryField = Entry(redditFrame)
   hoursCurrentlySetLabel = Label(redditFrame, textvariable=currentHoursToSave)
   setHoursButton = Button(
-      redditFrame,
-      text='Set Hours To Keep',
-      command=lambda: setHoursToSave(hoursEntryField.get(), currentHoursToSave)
+    redditFrame,
+    text='Set Hours To Keep',
+    command=lambda: setHoursToSave(hoursEntryField.get(), currentHoursToSave)
   )
 
   currentMaxScore = StringVar()
   currentMaxScore.set('Currently set to: 0 upvotes')
   maxScoreLabel = Label(
-      redditFrame, text='Delete comments/submissions less than score:')
+    redditFrame, text='Delete comments/submissions less than score:')
   maxScoreEntryField = Entry(redditFrame)
   maxScoreCurrentlySetLabel = Label(redditFrame, textvariable=currentMaxScore)
   setMaxScoreButton = Button(
-      redditFrame,
-      text='Set Max Score',
-      command=lambda: setMaxScore(maxScoreEntryField.get(), currentMaxScore)
+    redditFrame,
+    text='Set Max Score',
+    command=lambda: setMaxScore(maxScoreEntryField.get(), currentMaxScore)
   )
   setMaxScoreUnlimitedButton = Button(
-      redditFrame,
-      text='Set Unlimited',
-      command=lambda: setMaxScore('Unlimited', currentMaxScore)
+    redditFrame,
+    text='Set Unlimited',
+    command=lambda: setMaxScore('Unlimited', currentMaxScore)
   )
 
   deleteCommentsButton = Button(
-      redditFrame,
-      text='Delete comments',
-      command=deleteComments
+    redditFrame,
+    text='Delete comments',
+    command=deleteComments
   )
 
   deleteSubmissionsButton = Button(
-      redditFrame,
-      text='Delete submissions',
-      command=deleteSubmissions
+    redditFrame,
+    text='Delete submissions',
+    command=deleteSubmissions
   )
 
   showStateButton = Button(
-      redditFrame,
-      text='Show Options',
-      command=printState
+    redditFrame,
+    text='Show Options',
+    command=printState
   )
 
   hoursTextLabel.grid(row=0, column=0)
@@ -163,13 +210,15 @@ def buildRedditTab(redditFrame):
 def createUI():
   root = Tk()
   root.title('Social Scrubber')
-
+  
   tabs = ttk.Notebook(root)
+
+  loginFrame = ttk.Frame(tabs)
+  buildLoginTab(loginFrame)
+  tabs.add(loginFrame, text='Login To Accounts')
   
   redditFrame = ttk.Frame(tabs)
-  
   buildRedditTab(redditFrame)
-
   tabs.add(redditFrame, text='reddit')
 
   tabs.pack(expand=1, fill="both")
@@ -178,7 +227,6 @@ def createUI():
 
 
 def main():
-  setInitState()
   createUI()
 
 
