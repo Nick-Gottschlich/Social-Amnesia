5018d30bf5e5c1b115d5e670a08c60d7d7db4847
Nick Gottschlich <nickpgott@gmail.com>
2018-06-16 15:42:03 -0500

    added some options to get the package builder working on windows

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import sys
 base = None
 if sys.platform == "win32":
   base = "Win32GUI"
+  os.environ['TCL_LIBRARY'] = "C:\\Python36\\tcl\\tcl8.6"
+  os.environ['TK_LIBRARY'] = "C:\\Python36\\tcl\\tk8.6"
 build_exe_options = {"packages": ["os", "idna"]}
 setup(name="Social Amnesia",
       version="0.1.0",
