4ca51884fa7d22112bd9d7b367cfb804b45acf71
Nick Gottschlich <nickpgott@gmail.com>
2018-07-04 12:46:17 -0500

    added code for populators Im using to build test accounts

---
+++ populators/redditFaker.py
@@ -0,0 +1,33 @@
+#simple script to build up a bunch of comments and submissions on a test subreddit for a test user
+
+import praw
+from datetime import datetime
+from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
+
+USER_AGENT = 'RapidCommenter (by /u/JavaOffScript)'
+
+reddit = praw.Reddit(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent=USER_AGENT,
+    username=REDDIT_USERNAME,
+    password=REDDIT_PASSWORD
+)
+
+# https://www.reddit.com/r/socialamnesiatest/comments/8w39cr/imma_test_it/
+submission = reddit.submission(id='8w39cr')
+
+subreddit = reddit.subreddit('socialamnesiatest')
+
+while(1):
+    try:
+        submission.reply(datetime.now())
+        print('comment: ', datetime.now())
+    except Exception as e:
+        print(e)
+
+    try:
+        subreddit.submit(datetime.now(), selftext=datetime.now())
+        print('submission: ', datetime.now())
+    except Exception as e:
+        print(e)
---
+++ populators/twitterFaker.py
@@ -0,0 +1,29 @@
+# quick program to auto tweet and favorite as rapidly as possible to fill out a twitter acct
+
+import tweepy
+from datetime import datetime
+from secrets import twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret
+
+auth = tweepy.OAuthHandler(twitterConsumerKey, twitterConsumerSecret)
+auth.set_access_token(twitterAccessToken, twitterAccessTokenSecret)
+
+api = tweepy.API(auth)
+
+while(1):
+    # this emoji is used so much there will always be new tweets to favorite
+    testTweets = tweepy.Cursor(api.search, q='😂').items(10)
+
+    for tweet in testTweets:
+        try:
+            api.create_favorite(tweet.id)
+            print("FAVORITING: ", tweet.text)
+        except Exception as e:
+            print(e)
+        
+    while(1):
+        try:
+            api.update_status(datetime.now())
+            print('tweet tweet: ', datetime.now())
+        except Exception as e:
+            print(e)
+            break
