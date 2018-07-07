bd6e63e32a938f83a37ea130d0e458594f974eb0
Bartosz <elitarform@gmail.com>
2018-07-07 23:42:22 +0200

    Small fixes in test populators

--- populators/redditFaker.py
+++ populators/redditFaker.py
@@ -1,7 +1,10 @@
-#simple script to build up a bunch of comments and submissions on a test subreddit for a test user
-
+"""
+Simple script to build up a bunch of comments and submissions on a test subreddit for a test user
+"""
 import praw
 from datetime import datetime
+
+# TODO: Use `os.env`?
 from secrets import CLIENT_ID, CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD
 
 USER_AGENT = 'RapidCommenter (by /u/JavaOffScript)'
@@ -16,10 +19,9 @@ reddit = praw.Reddit(
 
 # https://www.reddit.com/r/socialamnesiatest/comments/8w39cr/imma_test_it/
 submission = reddit.submission(id='8w39cr')
-
 subreddit = reddit.subreddit('socialamnesiatest')
 
-while(1):
+while True:
     try:
         submission.reply(datetime.now())
         print('comment: ', datetime.now())
--- populators/twitterFaker.py
+++ populators/twitterFaker.py
@@ -1,15 +1,18 @@
-# quick program to auto tweet and favorite as rapidly as possible to fill out a twitter acct
+"""
+Quick program to auto tweet and favorite as rapidly as possible to fill out a twitter acct
+"""
 
 import tweepy
 from datetime import datetime
+
+# TODO: Use `os.env`?
 from secrets import twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret
 
 auth = tweepy.OAuthHandler(twitterConsumerKey, twitterConsumerSecret)
 auth.set_access_token(twitterAccessToken, twitterAccessTokenSecret)
-
 api = tweepy.API(auth)
 
-while(1):
+while True:
     # this emoji is used so much there will always be new tweets to favorite
     testTweets = tweepy.Cursor(api.search, q='😂').items(10)
 
@@ -19,8 +22,8 @@ while(1):
             print("FAVORITING: ", tweet.text)
         except Exception as e:
             print(e)
-        
-    while(1):
+
+    while True:
         try:
             api.update_status(datetime.now())
             print('tweet tweet: ', datetime.now())
