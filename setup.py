91745812474467793c110ec53f20ee42a8b2744a
Nick Gottschlich <nickpgott@gmail.com>
2019-01-20 15:03:22 -0600

    Version bump to 1.0.0 (#33)

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ app_description = 'Forget the past. Social Amnesia makes sure ' \
                   'your social media accounts only show your posts ' \
                   'from recent history, not from "that phase" 5 years ago.'
 
+
 def find_data_file(filename):
     if getattr(sys, 'frozen', False):
         datadir = os.path.dirname(sys.executable)
     else:
         datadir = os.path.dirname(__file__)
     return os.path.join(datadir, filename)
+
 
 base = None
 if sys.platform == 'win32':
@@ -23,9 +25,11 @@ if sys.platform == 'win32':
 
 build_exe_options = {'packages': ['os', 'idna', 'multiprocessing', 'dbm']}
 bdist_mac_options = {'iconfile': find_data_file('icon.icns')}
-executables = [Executable('SocialAmnesia.py', base=base, icon=find_data_file('icon.ico'))]
+executables = [Executable('SocialAmnesia.py', base=base,
+                          icon=find_data_file('icon.ico'))]
 
 setup(name=app_name,
+      version='1.0.0',
       description=app_description,
       options={'build_exe': build_exe_options, 'bdist_mac': bdist_mac_options},
       executables=executables)
