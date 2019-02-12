7423b77ecf7a3e66daea0958b7878c17824cff90
Nick Gottschlich <nickpgott@gmail.com>
2019-02-11 21:55:04 -0600

    2fa initial implementation working

--- services/reddit.py
+++ services/reddit.py
@@ -9,6 +9,8 @@ import tkinter.ttk as ttk
 from tkinter import messagebox
 import shelve
 import sys
+import random
+import socket
 sys.path.insert(0, "../utils")
 
 USER_AGENT = 'Social Amnesia (by /u/JavaOffScript)'
@@ -122,13 +124,63 @@ def set_reddit_login(username, password, client_id, client_secret, login_confirm
     :param reddit_state: dictionary holding reddit settings
     :return: none
     """
-    reddit = praw.Reddit(
-        client_id=client_id,
-        client_secret=client_secret,
-        user_agent=USER_AGENT,
-        username=username,
-        password=password
-    )
+
+    def receive_connection():
+        """
+        Wait for and then return a connected socket..
+        Opens a TCP connection on port 8080, and waits for a single client.
+        """
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(('localhost', 8080))
+        server.listen(1)
+        client = server.accept()[0]
+        server.close()
+        return client
+
+    def send_message(client, message):
+        """
+        Send message to client and close the connection.
+        """
+        print(message)
+        client.send('HTTP/1.1 200 OK\r\n\r\n{}'.format(message).encode('utf-8'))
+        client.close()
+
+    # reddit = praw.Reddit(
+    #     client_id=client_id,
+    #     client_secret=client_secret,
+    #     user_agent=USER_AGENT,
+    #     username=username,
+    #     password=password,
+    #     redirect_uri='http://localhost:8080',
+    # )
+
+
+    state = str(random.randint(0, 65000))
+    url = reddit.auth.url(['identity', 'history', 'read', 'edit'], state, 'permanent')
+    print('Now open this url in your browser: '+url)
+    sys.stdout.flush()
+
+    client = receive_connection()
+    data = client.recv(1024).decode('utf-8')
+    param_tokens = data.split(' ', 2)[1].split('?', 1)[1].split('&')
+    params = {key: value for (key, value) in [token.split('=')
+                                              for token in param_tokens]}
+
+    if state != params['state']:
+        send_message(client, 'State mismatch. Expected: {} Received: {}'
+                     .format(state, params['state']))
+        return 1
+    elif 'error' in params:
+        send_message(client, params['error'])
+        return 1
+
+    refresh_token = reddit.auth.authorize(params['code'])
+    send_message(client, 'Refresh token: {}'.format(refresh_token))
+
+
+    # print(reddit.auth.url(['identity'], '...', 'permanent'))
+    # reddit.auth.authorize(code)
 
     if praw_config_file_path.is_file():
         os.remove(praw_config_file_path)
