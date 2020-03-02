b967a7d2b070726720dc820529710d9e88bc51d2
Nick Gottschlich <nicholas.gottschlich@procore.com>
2020-03-01 21:28:28 -0600

    Adding electron

---
+++ ui-rewrite/src/components/TestComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="test">
+    {{ message }}
+    TEST
+    {{data}}
+  </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+// const test = new Vue({
+//   el: '#test',
+//   data: {
+//     message: 'wut',
+//   },
+// });
+
+@Component
+export default class Test extends Vue {
+  message = 'what up g';
+
+  data = ''
+
+  render() {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((data) => {
+        this.data = data;
+      });
+
+    console.log('data');
+  }
+}
+</script>
--- ui-rewrite/src/views/Home.vue
+++ ui-rewrite/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h2>wus poppin my g</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import TestComponent from '@/components/TestComponent.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    TestComponent,
   },
 };
 </script>
