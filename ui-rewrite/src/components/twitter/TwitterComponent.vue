bc9f02e67ecd16f122a2a8143d2940f54558ab98
Nick Gottschlich <nicholas.gottschlich@procore.com>
2020-04-04 21:39:35 -0500

    refactoring and work towards gathering tweets

--- ui-rewrite/src/components/twitter/TwitterComponent.vue
+++ ui-rewrite/src/components/twitter/TwitterComponent.vue
@@ -34,21 +34,80 @@ export default class TwitterComponent extends Vue {
       consumer_key: twitterApi.consumer_key,
       consumer_secret: twitterApi.consumer_secret
     });
-
-    const { BrowserWindow } = electron.remote;
-    let twitterApiWindow;
     let oauth_verifier;
     let userClient;
+    const { BrowserWindow } = electron.remote;
+    const mainWindow = electron.remote.getCurrentWindow();
+    const twitterApiWindow = new BrowserWindow({
+      parent: mainWindow
+      // modal: true
+    });
+
+    const gatherTweets = (verificationResponse, maxId) => {
+      const data = {
+        user_id: verificationResponse.user_id,
+        // can only do 200 per request, so we need to continually make requests until we run out of tweets
+        count: 200
+      };
+      if (maxId) {
+        data.maxId = String(maxId);
+      }
+      userClient.get("statuses/user_timeline", { data }).then(tweets => {
+        if (tweets.length === 0) {
+          return;
+        }
+
+        this.userTweets.push(tweets);
+        // console.log("usertweets", this.userTweets);
+        // console.log('-1', this.userTweets[0].slice(-1)[0].id)
+        const oldest = this.userTweets[0].slice(-1)[0].id;
+        console.log("oldest", oldest);
+        // for some reason maxId isn't working, oldes stays the same each time
+        debugger;
+        gatherTweets(verificationResponse, oldest);
+      });
+    };
+
+    const makeFollowUpRequest = initialResponse => {
+      client
+        .getAccessToken({
+          key: initialResponse.oauth_token,
+          secret: initialResponse.oauth_token_secret,
+          verifier: oauth_verifier
+        })
+        .then(verificationResponse => {
+          if (
+            !verificationResponse.oauth_token ||
+            !verificationResponse.oauth_token_secret
+          ) {
+            // login has failed, abort
+            this.loginMessage = "Failed to login to twitter!";
+            throw Error(verificationResponse);
+          }
+
+          userClient = new Twitter({
+            consumer_key: twitterApi.consumer_key,
+            consumer_secret: twitterApi.consumer_secret,
+            access_token_key: verificationResponse.oauth_token,
+            access_token_secret: verificationResponse.oauth_token_secret
+          });
+
+          gatherTweets(verificationResponse);
+
+          this.loggedIn = true;
+          this.loginMessage = `Logged in to twitter as ${verificationResponse.screen_name}`;
+          twitterApiWindow.close();
+        })
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.error(`Failed to login to twitter with error ${error}`);
+          twitterApiWindow.close();
+        });
+    };
 
     client
       .getRequestToken("https://google.com")
       .then(initialResponse => {
-        const mainWindow = electron.remote.getCurrentWindow();
-
-        twitterApiWindow = new BrowserWindow({
-          parent: mainWindow,
-          modal: true
-        });
         twitterApiWindow.loadURL(
           `https://api.twitter.com/oauth/authenticate?oauth_token=${initialResponse.oauth_token}`
         );
@@ -57,51 +116,12 @@ export default class TwitterComponent extends Vue {
             const searchParams = new URLSearchParams(url.slice(23));
             oauth_verifier = searchParams.get("oauth_verifier");
 
-            client
-              .getAccessToken({
-                key: initialResponse.oauth_token,
-                secret: initialResponse.oauth_token_secret,
-                verifier: oauth_verifier
-              })
-              .then((verificationResponse, error) => {
-                if (
-                  !verificationResponse.oauth_token ||
-                  !verificationResponse.oauth_token_secret
-                ) {
-                  // login has failed, abort
-                  this.loginMessage = "Failed to login to twitter!";
-                  throw Error(verificationResponse);
-                }
-
-                userClient = new Twitter({
-                  consumer_key: twitterApi.consumer_key,
-                  consumer_secret: twitterApi.consumer_secret,
-                  access_token_key: verificationResponse.oauth_token,
-                  access_token_secret: verificationResponse.oauth_token_secret
-                });
-
-                userClient
-                  .get("statuses/user_timeline", {
-                    user_id: verificationResponse.user_id,
-                    // can only do 200 per request, so this is where we need to continually make requests until we run out of tweets
-                    count: 200
-                  })
-                  .then(userTweets => {
-                    this.userTweets = userTweets;
-                    console.log('user tweets', userTweets)
-                    this.loggedIn = true;
-                    this.loginMessage = `Logged in to twitter as ${verificationResponse.screen_name}`;
-                    twitterApiWindow.close();
-                  });
-              })
-              .catch(error => {
-                console.error(`Failed to login to twitter with error ${error}`);
-                twitterApiWindow.close();
-              });
+            makeFollowUpRequest(initialResponse);
           }
         });
       })
       .catch(error => {
+        // eslint-disable-next-line no-console
         console.error(
           `Failed to load twitter api window with error: ${error}s`
         );
